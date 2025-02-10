43d577b896014055dfe35959c3520bb07daa561e
⋆˚࿔ Lavinia Mendonça 𝜗𝜚˚⋆ <laviniamendoncapro@gmail.com>
2025-02-10 11:49:44 -0300

    first commit

---
+++ ponderada-1.js
@@ -0,0 +1,16 @@
+
+var nota1 = 7.0;''
+var nota2 = 9.5;
+var nota3 = 8.0;
+
+var media = (nota1 + nota2 + nota3)/3; 
+
+while (media >= 0) {
+    if (media >= 7.0) {
+      console.log("APROVADO COM MÉDIA " + media.toFixed(2)+ ".");
+      } else {
+     console.log("REPROVADO COM MÉDIA " + media.toFixed(2) + ".");
+       } 
+    break 
+}
+
